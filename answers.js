b089695221dc58b81e6b2c2ebbc792f64ccb2772
Karl Soulière-Crépeau <karlysun@gmail.com>
2015-09-11 18:32:28 +0000

    Second function created

--- answers.js
+++ answers.js
@@ -17,3 +17,23 @@ if(yourString.length > 0){
     console.log("Nothing was entered!");
 
 }
+
+// This function will be used to find out the last letter of a string
+
+function returnLastCharacter(aString) {
+
+    return aString[aString.length - 1];
+    
+}
+
+var yourString = prompt("Enter something of your choice");
+
+if(yourString.length > 0){
+
+    returnLastCharacter(yourString);
+    
+} else {
+    
+    console.log("Nothing was entered!");
+
+}
