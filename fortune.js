69d312e236809d0fa5580b42de0d5ec93194d16c
Karl Soulière-Crépeau <karlysun@gmail.com>
2015-09-14 02:47:01 +0000

    Added a file called fortune.js, first commit didn't work

---
+++ fortune.js
@@ -0,0 +1,35 @@
+var fortune = Math.floor(Math.random()*10);
+
+switch(fortune) {
+    case 0:
+        console.log("Land is always on the mind of a flying bird.");
+        break;
+    case 1:
+        console.log("You cannot love life until you live the life you love.");
+        break;
+    case 2:
+        console.log("Your shoes will make you happy today.");
+        break;
+    case 3:
+        console.log("If you have something good in your life, don't let it go!");
+        break;
+    case 4:
+        console.log("You learn from your mistakes... You will learn a lot today.");
+        break;
+    case 5:
+        console.log("A chance meeting opens new doors to success and friendship.");
+        break;
+    case 6:
+        console.log("People are naturally attracted to you.");
+        break;
+    case 7:
+        console.log("Enjoy the good luck a companion brings you.");
+        break;
+    case 8:
+        console.log("Change can hurt, but it leads a path to something better.");
+        break;
+    case 9:
+        console.log("Hard work pays off in the future, laziness pays off now.");
+        break;
+    default:
+}
